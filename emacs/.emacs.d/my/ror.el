2cad2a789a9eb6b070f6b9240b7924732e273b95
Andrés Maneiro <amaneiro@icarto.es>
2013-07-18 17:00:15 +0200

    load automatically ruby-mode for .rake files

--- emacs/.emacs.d/my/ror.el
+++ emacs/.emacs.d/my/ror.el
@@ -1,2 +1,6 @@
 ;;active rinari minor mode in ruby-mode
 (add-hook 'ruby-mode-hook 'rinari-minor-mode)
+
+;; associate rake tasks to ruby-mode
+(setq auto-mode-alist
+      (cons '("\\.rake$" . ruby-mode) auto-mode-alist))
