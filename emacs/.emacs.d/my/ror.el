2d006ec44e99edeaff217cd0394bd2953a84e5cc
Andrés Maneiro <amaneiro@icarto.es>
2013-08-01 10:42:37 +0200

    Delete some RoR packages

--- emacs/.emacs.d/my/ror.el
+++ emacs/.emacs.d/my/ror.el
@@ -1,6 +1,3 @@
-;;active rinari minor mode in ruby-mode
-(add-hook 'ruby-mode-hook 'rinari-minor-mode)
-
 ;; associate rake tasks to ruby-mode
 (setq auto-mode-alist
       (cons '("\\.rake$" . ruby-mode) auto-mode-alist))
